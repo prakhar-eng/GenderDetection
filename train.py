c0b4c850d6edd9f06b52f297b0e1e3cdc5d74899
Prakhar Agrawal <prakhar79199@gmail.com>
2020-10-30 16:17:17 +0530

    tain.py

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ data = []
 labels = []
 
 # load image files from the dataset
-image_files = [f for f in glob.glob(r'C:\Files\gender_dataset_face' + "/**/*", recursive=True) if not os.path.isdir(f)]
+image_files = [f for f in glob.glob(r'F:\gender\Gender-Detection\gender_dataset_face' + "/**/*", recursive=True) if not os.path.isdir(f)]
 random.shuffle(image_files)
 
 # converting images to arrays and labelling the categories
